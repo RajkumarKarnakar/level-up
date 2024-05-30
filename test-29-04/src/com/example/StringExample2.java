37448f9870062c55f889b55d42eff8dc2c651fc2
planetofthecode <rajkumar.karnakar@outlook.com>
2024-05-30 16:56:56 +0530

    method for vowels is added

--- test-29-04/src/com/example/StringExample2.java
+++ test-29-04/src/com/example/StringExample2.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class StringExample2 {
     public static void main(String[] args) {
@@ -18,12 +19,25 @@ public class StringExample2 {
         System.out.println("------palindrome---------");
         Arrays.stream(words)
                 .filter(StringExample2::isPalindrome)
+//                .filter(wordForPalindrome -> IntStream.range(0, wordForPalindrome.length() / 2)
+//                        .allMatch(i -> wordForPalindrome.charAt(i)
+//                                == wordForPalindrome.charAt(wordForPalindrome.length() - 1 - i)))
                 .map(s -> s.trim().substring(0, 1).toUpperCase().concat(s.substring(1).toLowerCase()))
+                .forEach(System.out::println);
+//
+//        String wordForPalindrome = "race";
+//        System.out.println(wordForPalindrome + " is palindrome: " +
+//                IntStream.range(0, wordForPalindrome.length() / 2)
+//                        .allMatch(i -> wordForPalindrome.charAt(i)
+//                                == wordForPalindrome.charAt(wordForPalindrome.length() - 1 - i))
+//        );
+
+        System.out.println("------words with no vowels------");
+        Arrays.stream(words).filter(s -> countVowels(s)==0)
                 .forEach(System.out::println);
     }
 
     static boolean isPalindrome(String original) {
-        // Your code goes here.
         original = original.toLowerCase();
         for (int i = 0; i <= original.length() - 1; i++) {
             if (original.charAt(i) != original.charAt(original.length() - i - 1))
@@ -31,5 +45,15 @@ public class StringExample2 {
         }
 
         return true;
+    }
+    static int countVowels(String s) {
+        // Your code goes here.
+        int count = 0;
+        String vowels = "aeiouy";
+        for(char c : s.toCharArray()){
+            if(vowels.indexOf(c)!= -1)
+                count++;
+        }
+        return count;
     }
 }
